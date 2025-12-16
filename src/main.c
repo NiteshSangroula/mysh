35135db4be4aaa1ec4a5af2cb78610430d7af12e
NiteshSangroula <sangroulanitesh123@gmail.com>
2025-12-16 18:18:37 +0545

    feat: implement command parser

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "../include/parser.h"
+
 #define MAX_SIZE 100
 
 int main(int argc, char* argv[]) {
@@ -14,7 +16,15 @@ int main(int argc, char* argv[]) {
         printf("mysh> ");
         fgets(command, MAX_SIZE, stdin);
         command[strcspn(command, "\n")] = '\0';
-        printf("%s\n", command);
+
+        char* args[MAX_ARGS];
+        int length = parse_string(command, args);  // this changes the command
+
+        printf("[ ");
+        for (int i = 0; i <= length; i++) {
+            printf("%s ", args[i]);
+        }
+        printf("]\n");
     }
 
     return 0;
